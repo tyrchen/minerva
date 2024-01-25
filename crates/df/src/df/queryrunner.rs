545437c1de580e85a5d7f97a960ffc701f5d8b0d
Xin Wei <xinwei@tubi.tv>
2024-01-25 14:45:31 +0800

    Fixed failed build

--- crates/df/src/df/queryrunner.rs
+++ crates/df/src/df/queryrunner.rs
@@ -1,35 +1,34 @@
+use common::QueryRunner;
 use datafusion::dataframe::DataFrameWriteOptions;
 use datafusion::datasource::file_format::parquet::ParquetFormat;
 use datafusion::datasource::listing::ListingOptions;
-use datafusion::error::Result;
 use datafusion::prelude::*;
 use datafusion_common::{FileType, GetExt};
-use common::QueryRunner;
 
 use object_store::aws::AmazonS3Builder;
 use std::env;
 use std::sync::Arc;
 use url::Url;
 
-pub struct DFQueryRunner {
-    region: str,
-    bucket: str,
-    table: str,
-    src: str,
-    dst: str,
+pub struct DFQueryRunner<'a> {
+    region: &'a str,
+    bucket: &'a str,
+    table: &'a str,
+    src: &'a str,
+    dst: &'a str,
 }
 
-impl QueryRunner for DFQueryRunner {
+impl<'a> QueryRunner for DFQueryRunner<'a> {
     type Response = ();
-    type Error = ();
+    type Error = anyhow::Error;
 
     async fn query(&self, sql: &str) -> std::result::Result<Self::Response, Self::Error> {
         // create local execution context
         let ctx = SessionContext::new();
 
         let s3 = AmazonS3Builder::new()
-            .with_bucket_name(&self.bucket)
-            .with_region(&self.region)
+            .with_bucket_name(self.bucket)
+            .with_region(self.region)
             .with_access_key_id(env::var("AWS_ACCESS_KEY_ID").unwrap())
             .with_secret_access_key(env::var("AWS_SECRET_ACCESS_KEY").unwrap())
             .build()?;
@@ -44,25 +43,25 @@ impl QueryRunner for DFQueryRunner {
         let file_format = ParquetFormat::default().with_enable_pruning(Some(true));
         let listing_options = ListingOptions::new(Arc::new(file_format))
             .with_file_extension(FileType::PARQUET.get_ext());
-        ctx.register_listing_table(&self.table, &path, listing_options, None, None)
+        ctx.register_listing_table(self.table, &path, listing_options, None, None)
             .await?;
 
         // execute the query
         let df = ctx.sql(sql).await?;
 
-        let out_path = format!("s3://{bucket_name}/test_write/");
+        let out_path = format!("s3://{}/{}/", self.bucket, self.dst);
         df.clone()
             .write_parquet(&out_path, DataFrameWriteOptions::new(), None)
             .await?;
 
         //write as JSON to s3
-        let json_out = format!("s3://{bucket_name}/json_out");
+        let json_out = format!("s3://{}/json_out", self.bucket);
         df.clone()
             .write_json(&json_out, DataFrameWriteOptions::new())
             .await?;
 
         //write as csv to s3
-        let csv_out = format!("s3://{bucket_name}/csv_out");
+        let csv_out = format!("s3://{}/csv_out", self.bucket);
         df.write_csv(&csv_out, DataFrameWriteOptions::new(), None)
             .await?;
 
